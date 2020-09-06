948af4aff920bd636b126a27cad2242cddc17f3c
Negin <nuggetnegin@gmail.com>
2020-09-06 11:39:50 -0400

    added month availability

--- components/Bug.js
+++ components/Bug.js
@@ -5,12 +5,13 @@ export default function Bug({ data }) {
         return (
           <ul key={key}>
             <li>{data[key].name['name-USen']}</li>
-            <li>{data[key].price}</li>
             <li>
               <img src={data[key].image_uri}></img>
             </li>
+            <li>{data[key].availability['month-northern']}</li>
             <li>{data[key].availability['time']}</li>
             <li>{data[key].availability['rarity']}</li>
+            <li>{data[key].price}</li>
           </ul>
         );
       })}
